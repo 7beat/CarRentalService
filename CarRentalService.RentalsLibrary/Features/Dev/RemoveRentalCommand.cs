06798d7439af36e87c98184beff2fbac9b5b373d
Tomasz Krzemiński <7beat123@wp.pl>
2025-02-26 19:43:08 +0100

    Testing

---
+++ CarRentalService.RentalsLibrary/Features/Dev/RemoveRentalCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace CarRentalService.RentalsLibrary.Features.Dev;
+public record class RemoveRentalCommand(Guid Id) : IRequest<bool>;
+
+internal class RemoveRentalCommandHandler : IRequestHandler<RemoveRentalCommand, bool>
+{
+    public async Task<bool> Handle(RemoveRentalCommand request, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"@@ !!! Test Removing Rental with Id: {request.Id} !!! @@");
+        return true;
+    }
+}
