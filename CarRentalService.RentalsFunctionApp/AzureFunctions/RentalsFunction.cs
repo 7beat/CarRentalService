516357f81f16e6df7ad2698b9c6b083c2bf42c0d
Tomasz Krzemiński <7beat123@wp.pl>
2025-03-01 18:20:29 +0100

    Improvements

--- CarRentalService.RentalsFunctionApp/AzureFunctions/RentalsFunction.cs
+++ CarRentalService.RentalsFunctionApp/AzureFunctions/RentalsFunction.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using CarRentalService.CommonLibrary.Constants;
 using CarRentalService.RentalsLibrary.Constants;
 using CarRentalService.RentalsLibrary.Features.Dev;
@@ -16,7 +17,14 @@ public class RentalsFunction(ILogger<RentalsFunction> logger, IMediator mediator
     [Function(nameof(Test))]
     public async Task<IActionResult> Test([HttpTrigger(AuthorizationLevel.Function, TriggerHttpMethods.Get, TriggerHttpMethods.Post)] HttpRequestData req)
     {
-        await mediator.Send(new TestCommand(2, "John"));
+        var requestBody = await req.ReadAsStringAsync();
+        if (!string.IsNullOrEmpty(requestBody))
+        {
+            var command = JsonSerializer.Deserialize<TestCommand>(requestBody);
+            await mediator.Send(command);
+        }
+
+        //await mediator.Send(new TestCommand(2, "John"));
         logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
     }
