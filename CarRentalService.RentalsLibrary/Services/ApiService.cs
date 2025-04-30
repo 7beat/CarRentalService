b4feb99d4ba45d95ffaf0a00c8a34e05437ca317
Tomasz Krzemiński <7beat123@wp.pl>
2025-04-30 21:59:55 +0200

    Added ApiService

---
+++ CarRentalService.RentalsLibrary/Services/ApiService.cs
@@ -0,0 +1,23 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+
+namespace CarRentalService.RentalsLibrary.Services;
+public class ApiService
+{
+    private static AsyncCircuitBreakerPolicy circuitBreakerPolicy;
+    private static AsyncRetryPolicy retryPolicy;
+
+    public ApiService()
+    {
+        retryPolicy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(2, sd => TimeSpan.FromSeconds(20));
+
+        circuitBreakerPolicy = Policy
+            .Handle<Exception>()
+            .CircuitBreakerAsync(
+            exceptionsAllowedBeforeBreaking: 3,
+            durationOfBreak: TimeSpan.FromSeconds(30));
+    }
+}
